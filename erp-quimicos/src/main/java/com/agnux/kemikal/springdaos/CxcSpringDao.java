ceb13b9d2bde2d081ba838bba4e8fa7e727025a5
Bob Ross <pianodaemon@gmail.com>
2018-09-18 00:07:26 -0500

    PAG-COMP : Java side dao and interface
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- erp-quimicos/src/main/java/com/agnux/kemikal/springdaos/CxcSpringDao.java
+++ erp-quimicos/src/main/java/com/agnux/kemikal/springdaos/CxcSpringDao.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -30,8 +32,20 @@ public class CxcSpringDao implements CxcInterfaceDao{
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
-
+    @Override
+    public String q_serie_folio(final Integer usr_id) {
+        String SQL = "select fac_cfds_conf_folios.serie as serie, " +
+            "fac_cfds_conf_folios.folio_actual::character varying as folio " +
+            "FROM gral_suc AS SUC " +
+            "LEFT JOIN fac_cfds_conf ON fac_cfds_conf.gral_suc_id = SUC.id " +
+            "LEFT JOIN fac_cfds_conf_folios ON fac_cfds_conf_folios.fac_cfds_conf_id = fac_cfds_conf.id " +
+            "LEFT JOIN gral_usr_suc AS USR_SUC ON USR_SUC.gral_suc_id = SUC.id " +
+            "WHERE fac_cfds_conf_folios.proposito = \'PAG\' " +
+            "AND USR_SUC.gral_usr_id=" + usr_id;
+        Logger.getLogger(FacturasSpringDao.class.getName()).log(Level.INFO, SQL);
+        Map<String, Object> map_iva = this.getJdbcTemplate().queryForMap(SQL);
+        return (map_iva.get("serie").toString() + map_iva.get("folio").toString());
+    }
 
     @Override
     public HashMap<String, String> selectFunctionValidateAaplicativo(String data, Integer idApp, String extra_data_array) {
